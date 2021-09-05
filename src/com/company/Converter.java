97538eb95dd5cbece6f888258fecdef6997f535d
AiRisska <airin-l@yandex.ru>
2021-09-05 12:05:08 +0300

    Initial commit

---
+++ src/com/company/Converter.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Converter {
+
+    double rateToday;
+    String valuta;
+    Scanner scanner = new Scanner(System.in);
+// ПОМНИ: double число пишется через запятую!
+    void convert(double rubles) {
+            System.out.println("Напишите в какой вид валюты хотите конвертировать?");
+            valuta = scanner.nextLine();
+            System.out.println("Какой у нее сегодня курс?");
+            rateToday = scanner.nextDouble();
+            System.out.println("Ваши сбережения в валюте "+valuta+ " составляют: " + rubles / rateToday);
+    }
+}
+
